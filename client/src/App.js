d41b15b79bcbac8103d118501930daf4c71ddf5c
JUSTFIDEL <fidenki1429@gmail.com>
2023-03-01 17:44:31 +0100

    logo and images for featured page

--- client/src/App.js
+++ client/src/App.js
@@ -4,27 +4,40 @@ function App() {
 	return (
 		<div className="App">
 			<header>
-				<a href="/">justFidel</a>
+				<a href="/">
+					<img
+						src="./images/logo.svg"
+						className="logo"
+						alt="logo"
+					/>
+				</a>
 			</header>
 
 			<main className="margin">
 				<h1>Featured Products</h1>
-				<div className="containner">
+				<div className="products">
 					{data.products.map(product => (
-						<div
-							key={product.slug}
-							className="image-containner">
-							<img
-								alt={product.name}
-								src={product.image}
-								className="img-wd"
-							/>
-							<p className="p-name">
-								{product.name}
-							</p>
-							<p className="p-price">
-								N{product.price}
-							</p>
+						<div key={product.slug} className="product">
+							<a href={'/product/{product.slug'}>
+								<img
+									alt={product.name}
+									src={product.image}
+								/>
+							</a>
+							<div className="product-info">
+								<a
+									href={
+										'/product/{product.slug'
+									}>
+									<p className="p-name">
+										{product.name}
+									</p>
+								</a>
+								<p className="p-price">
+									N{product.price}
+								</p>
+								<button>Add to cart</button>
+							</div>
 						</div>
 					))}
 				</div>
