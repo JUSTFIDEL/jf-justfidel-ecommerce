36c18efa29ac00603905963823da73a77bb68bdc
JUSTFIDEL <fidenki1429@gmail.com>
2023-04-10 15:38:22 +0100

    express

--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
-import express from 'express'
+// import express from 'express'
+const express = require('express')
 import path from 'path'
 import mongoose from 'mongoose'
 import dotenv from 'dotenv'
